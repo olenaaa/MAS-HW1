843a74ded46187d3e370c229951a19af91e47974
Olena Buriakova <olena.buriakova@gmail.com>
2023-01-25 07:24:32 -0500

    create login & logout func

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:app2/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'login_page.dart';
+// import 'login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: MainPage(),
       // const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
